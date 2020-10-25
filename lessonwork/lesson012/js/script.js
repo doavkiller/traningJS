92555a559d613b5648e31cb845e383a9bd28c3a7
doavkiller <ceps2@yandex.ru>
2020-10-26 01:00:01 +0300

    the end lesson 15

--- lessonwork/lesson012/js/script.js
+++ lessonwork/lesson012/js/script.js
@@ -10,12 +10,55 @@ const personalMovieDB = {
     privat: false
 };
 
-for (let i = 0; i < 2; i++) {
+for (let i = 1; i < 3; i++) {
     const lastMovieFilm = 
     prompt('Один из последних просмотренных фильмов?', ''),
     lastMovieRating = prompt('Оцените этот фильм', '');
-    personalMovieDB.movies[lastMovieFilm] = +lastMovieRating;
+    
+    if (lastMovieFilm != null  && lastMovieRating != 
+        null && lastMovieFilm != "" && 
+        lastMovieRating != "" && lastMovieFilm.length < 50) {
+            personalMovieDB.movies[lastMovieFilm] = +lastMovieRating;
+    } else {
+        i--;
+    }
 }
 
+let i = 1;
+// while (i < 3) {
+//     const lastMovieFilm = 
+//     prompt('Один из последних просмотренных фильмов?', ''),
+//     lastMovieRating = prompt('Оцените этот фильм', '');
+//     if (lastMovieFilm != null  && lastMovieRating != 
+//         null && lastMovieFilm != "" && 
+//         lastMovieRating != "" && lastMovieFilm.length < 50) {
+//             personalMovieDB.movies[lastMovieFilm] = +lastMovieRating;
+//             i++;
+//     } 
+// }
 
+// do {
+//     const lastMovieFilm = 
+//     prompt('Один из последних просмотренных фильмов?', ''),
+//     lastMovieRating = prompt('Оцените этот фильм', '');
+//     if (lastMovieFilm != null  && lastMovieRating != 
+//         null && lastMovieFilm != "" && 
+//         lastMovieRating != "" && lastMovieFilm.length < 50) {
+//             personalMovieDB.movies[lastMovieFilm] = +lastMovieRating;
+//             i++;
+//     } 
+// } while (i < 3);
+
+
+if (numberOfFilms != null && numberOfFilms != "" && numberOfFilms < 10) {
+ alert('Просмотрено довольно мало фильмов', '');
+} else if(numberOfFilms != null && numberOfFilms != "" && 
+numberOfFilms >= 10 &&  numberOfFilms < 30) {
+    alert('Вы класический зритель', '');
+} else if(numberOfFilms != null && numberOfFilms != "" && numberOfFilms >= 30) {
+    alert('Вы киноман', '');
+}
+else {
+    alert('ошибка');
+}
 console.log(personalMovieDB);
