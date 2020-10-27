3bbfe678ba7a3079876ae87c498ab685d319aca2
doavkiller <ceps2@yandex.ru>
2020-10-27 04:03:56 +0300

    ЗАВЕРШЕНИЕ ДЕСТРУКТУЛИЗАЦИИ ОБЬЕКТОВ

--- lessonwork/lessons/js/script.js
+++ lessonwork/lessons/js/script.js
@@ -1,2 +1,14 @@
 'use strict';
+const options = {
+    name: 'test',
+    width: 1024,
+    height: 1929,
+    color: {
+        border: 'black',
+        bg: 'white'
+    }
+};
+
+const {border, bg} = options.color;
+console.log(border + ' : ' + bg);
 
